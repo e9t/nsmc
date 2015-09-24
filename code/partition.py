4f9835d513ff8b64bbc1bcb0a14a4d7fd3c09395
Lucy Park <me@lucypark.kr>
2015-09-25 05:17:41 +0900

    Add code used for partitioning

---
+++ code/partition.py
@@ -0,0 +1,16 @@
+#! /usr/bin/python
+# -*- coding: utf-8 -*-
+
+import numpy as np; np.random.seed(1234)
+import pandas as pd
+
+
+ntrain =150000
+
+data = pd.read_csv('../ratings.txt', sep='\t', quoting=3)
+data = pd.DataFrame(np.random.permutation(data))
+trn, tst = data[:ntrain], data[ntrain:]
+
+header = 'id document label'.split()
+trn.to_csv('../ratings_train.txt', sep='\t', index=False, header=header)
+tst.to_csv('../ratings_test.txt', sep='\t', index=False, header=header)
